78933496e68bb0a720772563f21c9446094f6f4e
Shelton Louis <louisshelton0@gmail.com>
2025-10-08 14:39:20 -0400

    style(repo): apply formatting to vite.config.ts and lockfile

--- packages/nx-tsup/vite.config.ts
+++ packages/nx-tsup/vite.config.ts
@@ -3,7 +3,17 @@ import dts from 'vite-plugin-dts';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import path from 'node:path';
 
-const external = [/^@nx\//, '@nx/devkit', 'tslib', 'node:path', 'node:fs', 'node:child_process', 'readline', 'assert', 'enquirer'];
+const external = [
+  /^@nx\//,
+  '@nx/devkit',
+  'tslib',
+  'node:path',
+  'node:fs',
+  'node:child_process',
+  'readline',
+  'assert',
+  'enquirer',
+];
 
 export default defineConfig({
   build: {
