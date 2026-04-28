0e09e3da79b241845dbf88b74b1e027cef7d9572
louiss0 <louisshelton0@gmail.com>
2026-04-28 14:31:33 -0400

    refactor(pi-package): restart project!
    I know that this project does work! But now I need to take over and set it in the right
    direction!
    The problem with this project is that I don't know when and where things come from!

--- packages/create-pi-package/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { createPiPackage } from './lib/create-pi-package.js';
-export { runCli } from './lib/run-cli.js';
-export type {
-  CreatePiPackageOptions,
-  CreatePiPackageResult,
-} from './lib/types.js';
