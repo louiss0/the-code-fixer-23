2007f9d08090a287517629c7cee826f55e828630
louiss0 <louisshelton0@gmail.com>
2026-04-28 16:17:35 -0400

    fix(create-pi-package): move commander typings to package

--- packages/create-pi-package/src/index.spec.ts
+++ packages/create-pi-package/src/index.spec.ts
@@ -1,23 +1,27 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from "vitest";
 
-import { shouldRunCli } from './index';
+import { runCli, shouldRunCli } from "./index";
 
-describe('shouldRunCli', () => {
-  it('skips the cli while running in development', () => {
-    expect(
-      shouldRunCli({ DEV: true, MODE: 'development' } as ImportMetaEnv),
-    ).toBe(false);
+describe("runCli", () => {
+  it("returns the configured commander program", () => {
+    const result = runCli();
+
+    vi.spyOn(result, "action");
+
+    expect(result.).toBeCalled();
+  });
+});
+
+describe("shouldRunCli", () => {
+  it("skips the cli while running in development", () => {
+    expect(shouldRunCli({ DEV: true, MODE: "development" } as ImportMetaEnv)).toBe(false);
   });
 
-  it('runs the cli for production builds', () => {
-    expect(
-      shouldRunCli({ DEV: false, MODE: 'production' } as ImportMetaEnv),
-    ).toBe(true);
+  it("runs the cli for production builds", () => {
+    expect(shouldRunCli({ DEV: false, MODE: "production" } as ImportMetaEnv)).toBe(true);
   });
 
-  it('skips the cli while running tests', () => {
-    expect(shouldRunCli({ DEV: false, MODE: 'test' } as ImportMetaEnv)).toBe(
-      false,
-    );
+  it("skips the cli while running tests", () => {
+    expect(shouldRunCli({ DEV: false, MODE: "test" } as ImportMetaEnv)).toBe(false);
   });
 });
--- packages/create-pi-package/src/index.ts
+++ packages/create-pi-package/src/index.ts
@@ -1,15 +1,21 @@
-import { Command } from 'commander';
+import { Command } from "@commander-js/extra-typings";
 
-export function runCli() {
+const newLocal: Parameters<Command["action"]>[0] = function (value) {
+  console.log(value);
+};
+export function runCli(...args: string[]) {
   const program = new Command();
 
-  program.parse(process.argv);
+  return program
+    .option("-e --extension", "Do you want to create a package with an extension?")
+    .action(newLocal)
+    .parse(args);
 }
 
 export function shouldRunCli(environment: ImportMetaEnv) {
-  return !environment.DEV && environment.MODE !== 'test';
+  return !environment.DEV && environment.MODE !== "test";
 }
 
 if (shouldRunCli(import.meta.env)) {
-  runCli();
+  runCli(...process.argv.slice(2));
 }
