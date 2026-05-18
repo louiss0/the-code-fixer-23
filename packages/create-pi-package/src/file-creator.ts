c4c61251e4a8832ea4442c2dbc90b2958ca02ad4
louiss0 <louisshelton0@gmail.com>
2026-05-18 19:24:31 -0400

    refactor(create-pi-package): remove unused fs and path imports
    Removed unused imports for `mkdirSync` from `node:fs` and `path` from `node:path`.

--- packages/create-pi-package/src/file-creator.ts
+++ packages/create-pi-package/src/file-creator.ts
@@ -1,5 +1,5 @@
-import { mkdirSync, writeFileSync } from "node:fs";
-import path, { resolve } from "node:path";
+import { writeFileSync } from "node:fs";
+import { resolve } from "node:path";
 import type { AllowedFolderChioceValues, AllowedTestRunnerChioces } from "./options";
 
 const extensionContent = `export default function (pi:ExtensionAPI) {
